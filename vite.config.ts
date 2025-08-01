eee4cb70745738b899c034045c49f54979645d20
Gaddam Revanth <gaddamrevanthr@gmail.com>
2025-08-01 14:01:37 +0530

    Update Vercel configuration and root directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Intern-Dashboard/', // Set the base path for GitHub Pages
+
   server: {
     host: "::",
     port: 8080,
